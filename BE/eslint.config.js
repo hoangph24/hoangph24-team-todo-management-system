97e906dc678f58b118cee72cce2ae578778515a0
hoangphamhuy24 <hoangpham.huy@aircanada.ca>
2025-08-06 06:05:46 +0700

    feat: Configure TypeScript and ESLint for both backend and frontend

---
+++ BE/eslint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    'prettier/prettier': 'error',
+  },
+  ignorePatterns: ['dist/**', 'node_modules/**', 'coverage/**'],
+}; 
